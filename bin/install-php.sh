c5e37ca024e69cdd5f9a89a4b907c6652e2d9716
Joshua Mabina <mabinajoshua@gmail.com>
2017-09-07 05:28:58 +0300

    Create install-php script
    Builds and installs the latest PHP version.

---
+++ bin/install-php.sh
@@ -0,0 +1,82 @@
+#!/bin/env bash
+
+sudo -v
+
+# Clean up!
+sudo apt-get remove --purge php7.0*
+sudo apt-get autoremove
+
+# Dependencies
+sudo apt-get update
+sudo apt-get install -y \
+    build-essential \
+    pkg-config \
+    git-core \
+    autoconf \
+    bison \
+    libxml2-dev \
+    libbz2-dev \
+    libmcrypt-dev \
+    libicu-dev \
+    libssl-dev \
+    libcurl4-openssl-dev \
+    libltdl-dev \
+    libjpeg-dev \
+    libpng-dev \
+    libpspell-dev \
+    libreadline-dev
+
+cd /tmp
+
+# Get the latest PHP source
+git clone -b 'php-7.1.9' --single-branch --depth=1 https://github.com/php/php-src.git
+
+cd ./php-src
+
+./buildconf --force
+
+CONFIGURE_STRING="--prefix=/usr/local/php7 \
+                  --enable-huge-code-pages \
+                  --with-config-file-scan-dir=/usr/local/php7/etc/conf.d \
+                  --without-pear \
+                  --enable-bcmath \
+                  --with-bz2 \
+                  --enable-calendar \
+                  --enable-intl \
+                  --enable-exif \
+                  --enable-dba \
+                  --enable-ftp \
+                  --with-gettext \
+                  --with-gd \
+                  --with-jpeg-dir \
+                  --enable-mbstring \
+                  --with-mcrypt \
+                  --with-mhash \
+                  --enable-mysqlnd \
+                  --with-mysql=mysqlnd \
+                  --with-mysql-sock=/var/run/mysqld/mysqld.sock \
+                  --with-mysqli=mysqlnd \
+                  --with-pdo-mysql=mysqlnd \
+                  --with-openssl \
+                  --enable-pcntl \
+                  --with-pspell \
+                  --enable-shmop \
+                  --enable-soap \
+                  --enable-sockets \
+                  --enable-sysvmsg \
+                  --enable-sysvsem \
+                  --enable-sysvshm \
+                  --enable-wddx \
+                  --with-zlib \
+                  --enable-zip \
+                  --with-readline \
+                  --with-curl \
+                  --enable-fpm \
+                  --with-fpm-user=www-data \
+                  --with-fpm-group=www-data"
+
+./configure $CONFIGURE_STRING
+
+make -j `cat /proc/cpuinfo | grep processor | wc -l`
+
+sudo make install
