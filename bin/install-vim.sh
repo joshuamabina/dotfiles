bb2656723c17d179518d7dcfb3f0b0b11b7e1771
Joshua Mabina <mabinajoshua@gmail.com>
2017-11-07 09:44:22 +0300

    Add prerequisites

--- bin/install-vim.sh
+++ bin/install-vim.sh
@@ -1,30 +1,53 @@
 #!/usr/bin/env bash
 
-cd "$HOME/toolkit"
+##
+#Compile and install latest stable vim
+#
+#Contact mabinajoshua-at-gmail-dot-com
+##
 
-git clone git@github.com:vim/vim.git --depth=1
+sudo -v
+
+#prerequisite libraries
+apt-get install libncurses5-dev \
+    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+    python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git
+    
+#remove vim if you have it already.
+apt-get remove --purge vim \
+	vim-runtime \
+	gvim \
+	vim-tiny \
+	vim-common \
+	vim-gui-common
+	vim-nox
+
+cd /tmp
+
+git clone git@github.com:vim/vim.git --depth=1 \
+	&& cd vim
 
 ./configure \
---with-features=huge \
---enable-python3interp \
---enable-pythoninterp \
---with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu/ \
---enable-rubyinterp \
---enable-luainterp \
---enable-perlinterp \
---with-python3-config-dir=/usr/lib/python3.4/config-3.4m-x86_64-linux-gnu/ \
---enable-gui=no \
---enable-multibyte \
---enable-fontset \
---enable-cscope \
---disable-sysmouse \
---with-compiledby="Joshua Mabina <joshuamabina.github.io>"
+	--with-features=huge \
+	--enable-python3interp \
+	--enable-pythoninterp \
+	--with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu/ \
+	--enable-rubyinterp \
+	--enable-luainterp \
+	--enable-perlinterp \
+	--with-python3-config-dir=/usr/lib/python3.4/config-3.4m-x86_64-linux-gnu/ \
+	--enable-gui=no \
+	--enable-multibyte \
+	--enable-fontset \
+	--enable-cscope \
+	--disable-sysmouse
 
 make
 
-sudo make install
+make install
 
-mkdir ~/.vim && \
+vim --version
+
+mkdir -p ~/.vim && \
 	git clone git@github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# TODO: vim -e +PluginInstall
