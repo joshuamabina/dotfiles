7fed3d72defe030adc2226c7f116b967e3e05e05
Joshua Mabina <mabinajoshua@gmail.com>
2017-11-21 16:16:36 +0300

    force heroku to use ssh transport

--- bin/install-heroku.sh
+++ bin/install-heroku.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+#Download and install heroku-cli
 wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
+
+#Always use heroku on this machine
+git config --global url.ssh://git@heroku.com/.insteadOf https://git.heroku.com/
+
+#Add ssh-keys to heroku
+heroku keys:add
