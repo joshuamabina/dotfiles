1348682762dc61f338586c3ba2a8732f5b1d7bd8
Joshua Mabina <mabinajoshua@gmail.com>
2018-02-05 03:05:06 +0300

    Update TODOs

--- bin/install-laravel.sh
+++ bin/install-laravel.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+#TODO test for laravel environment
+#TODO install all necessary dependencies
+
 composer global require "laravel/installer"
--- bin/install-phpunit.sh
+++ bin/install-phpunit.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+#TODO install phpunit dependencies i.e. ext-dom
+
 composer global require --dev phpunit/phpunit
 
 #TODO alias pest=phpunit
