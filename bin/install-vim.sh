fdaf736cc5b3ab9535e9eaca88ad2feba33ef6c1
Lupyana <lupyanambembati@gmail.com>
2018-07-16 16:33:12 +0300

    automatically confirm yes on running apt-commands

--- bin/install-vim.sh
+++ bin/install-vim.sh
@@ -10,12 +10,12 @@
 sudo apt-get install libncurses5-dev \
     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
     libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
-    python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git
+    python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git -y
 
 ##When you want to build a program from source and it fails due to missing headers.
 ##Auto-apt can search what package would provide the header files.
 ##https://help.ubuntu.com/community/AutoApt
-sudo apt-get install auto-apt
+sudo apt-get install auto-apt -y
 
 ##CheckInstall keeps track of all the files created or modified by your installation script,
 ##builds a standard binary package (.deb, .rpm, .tgz) and
