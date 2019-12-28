2091aec86bcc4caea18e49964e2b0bacd78e0dda
Joshua Mabina <mabinajoshua@gmail.com>
2019-12-28 09:29:08 +0300

    fix inotify watches
    added a script to increase the number of inotify watches.
    
    resources:
    
    - See https://linux.die.net/man/1/inotifywatch
    - See https://github.com/guard/listen/wiki/Increasing-the-amount-of-inotify-watchers

---
+++ bin/fix_inotify_watches.sh
@@ -0,0 +1,3 @@
+echo 99999999 | sudo tee -a /proc/sys/fs/inotify/max_user_watches
+echo 99999999 | sudo tee -a /proc/sys/fs/inotify/max_queued_events
+echo 99999999 | sudo tee -a /proc/sys/fs/inotify/max_user_instances
